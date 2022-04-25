f89386863ce81bf3c073a495f402eb8e40b12f5b
Abdullah90 <darkside944@gmail.com>
2022-04-25 22:58:24 +0300

    fix evaluation with the judge & to get avrage dirctly

--- judge/serializersJudge.py
+++ judge/serializersJudge.py
@@ -10,17 +10,20 @@ class JudgeCreateSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def create(self, validated_data):
-        defaultCriteria = []
-
+        defCriteria = []
         for criteria in validated_data["evaluation"].project.criteria.all():
-            defaultCriteria.append()(
-                {"name": criteria.name, "weight": criteria.weight, "grade": 0}
+            defCriteria.append(
+                {
+                    "criteria_id": criteria.id,
+                    "criteria_name": criteria.name,
+                    "criteria_weight": criteria.weight,
+                    "grade": 0,
+                }
             )
-
         teams = []
         for team in validated_data["evaluation"].project.team.all():
             teams.append(
-                {"teamId": team.id, "teamName": team.name, "grade": defaultCriteria}
+                {"team_id": team.id, "team_name": team.name, "grade": defCriteria}
             )
         validated_data["grade"] = teams
         return super().create(validated_data)
