7c60ae4a6dde5148a834e7b35cab0d40e35cde72
Abdullah90 <darkside944@gmail.com>
2022-04-27 09:33:48 +0300

    change weight from charfield to int

--- criteria/modelsCriteria.py
+++ criteria/modelsCriteria.py
@@ -4,4 +4,4 @@ from django.db import models
 # ? what are the filed to show in the Criteria:
 class Criteria(models.Model):
     name = models.CharField(max_length=255)
-    weight = models.CharField(max_length=255)
+    weight = models.IntegerField()
